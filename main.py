571165218734209e0777c3e82ecae42e231a70e9
avitalmah <73911258+avitalmah@users.noreply.github.com>
2021-04-13 21:39:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ A = SquareMatrix([[1, 2, 4],
                   [3, -1, 1], ], "A")
 b = Matrix([[7], [8], [3]], "b")
 
-if (A.num_of_rows > 4):
+if (A.num_of_rows >= 4):
     L, U = A.LU()
     print(L)
     print(U)
